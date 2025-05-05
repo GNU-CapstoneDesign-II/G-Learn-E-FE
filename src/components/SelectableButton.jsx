54688a072da2f43842e191dce9afb7429b991104
JiWoo11 <118912948+JiWoo11@users.noreply.github.com>
2025-05-06 08:18:27 +0900

    merge fix

--- src/components/SelectableButton.jsx
+++ src/components/SelectableButton.jsx
@@ -3,16 +3,10 @@ import React from 'react';
 const SelectableButton = ({ label, isActive, onClick }) => {
   return (
     <button
-<<<<<<< HEAD
       className={`px-6 py-3 border-[1.5px] rounded-full cursor-pointer transition-all duration-200
         ${isActive
           ? 'bg-brown text-white font-bold border-brown'
           : 'bg-white text-darkbrown border-lightbrown hover:bg-lightbrown hover:text-white'
-=======
-      className={`px-6 py-3 border-[1.5px] rounded-full cursor-pointer transition-all ${isActive
-        ? 'bg-brown text-white font-bold border-brown'
-        : 'bg-white text-darkbrown border-lightbrown'
->>>>>>> develop
         }`}
       onClick={onClick}
     >
