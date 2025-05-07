2d4a9b192abe1983dab650793271bf0a5a9be694
canotlivewithoutyou <kes23529@gmail.com>
2025-05-07 15:46:27 +0900

    상단바 위치 이동 오류 수정

--- src/pages/FolderPage.jsx
+++ src/pages/FolderPage.jsx
@@ -1,3 +1,6 @@
+/* ──────────────────────────────────────────────────────────────
+   src/pages/FolderPage.jsx
+────────────────────────────────────────────────────────────── */
 import React, { useState } from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
